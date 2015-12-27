735fb829f80b4c6a3bdfffce1a04637c9333f3fe
Mark Danielson <mdanielson924@gmail.com>
2015-12-27 11:18:58 -0500

    Programming Assignment 2 Week 3 Class # rprog-035

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching heavy compution (matrix inverse)
 
-## Write a short comment describing this function
 
+## We create a special object we can then use to cache the results of a matrix inverse.  
 makeCacheMatrix <- function(x = matrix()) {
-
+    v <- NULL
+    set <- function(m){
+        x <<- m
+        v <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) v <<- solve
+    getinv <- function() v
+    list(set = set, get = get,
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Given the special object we created in 'makeCacheMatrix' we can either solve the inverse or recover the previously computed inverse of a matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    v <- x$getinv()
+    if(!is.null(v)){
+        message("getting cached data")
+        return(v)
+    }
+    data <- x$get()
+    v <- solve(data)
+    x$setinv(v)
+    v
+}  
+
